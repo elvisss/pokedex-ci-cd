c069c90706fd2e69acc24d7ba88846ee20d4ab6d
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2022-09-29 23:44:06 -0500

    cypress supportFile to false

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       config.pageLoadTimeout = 6000000
+      config.supportFile = false
       return config
     },
   },
