3f7ba76d22240dba39456c93c5e94ebb24a2aaab
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2022-10-02 15:26:56 -0500

    adding vercel.json

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const { version } = require('./package.json')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send({ version })
+})
 
 app.use(express.static('dist'))
 
+const PORT = process.env.PORT || 5000
+
 app.listen(PORT, () => {
-  console.log(`server started on port ${5000}`)
+  console.log(`server started on port ${PORT}`)
 })
+
+module.exports = app
